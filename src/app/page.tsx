805b5736f5cc37f09b94e292a9d0ac02d88290f1
kupendrav <kupendrav99@gmail.com>
2024-06-30 14:12:48 +0530

    feat: Add favicon to the landing page
    This commit adds a favicon to the landing page by including a link tag in the HTML. The favicon file "vre.png" is located in the "/logo" directory.
    
    The addition of the favicon enhances the user experience by providing a visual representation of the website in browser tabs and bookmarks.
    
    Co-authored-by: [Author Name]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,16 @@ import Image from "next/image";
 export default function Home() {
   const emailAddress = "myvidaspace@gmail.com";
   const mailtoLink = `mailto:${emailAddress}`;
-  
+
   return (
+    <div>
+      <link rel="icon" href="/logo/vre.png" />
     <div className="flex items-center gap-1 justify-center w-screen mt-4 grid place-items-center	h-56">
       <Image src="/logo/vida.jpeg" alt="Vida" width={200} height={200} />
       <h1 className="text-4xl">Vida</h1>
       <p>Coming soon you can contact us and give feedback and share your ideas </p>
       <a href={mailtoLink}>myvidaspace@gmail.com</a>
+    </div>
     </div>
   );
 }
